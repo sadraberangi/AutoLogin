4536c6b09c95f7b51af7a93332d9f15492e7e1aa
sadrabera <s.berangi@mail.sbu.ac.ir>
2022-08-04 18:54:39 +0430

    this is code of my auto login to slms.sbu.ac.ir

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+
+from pynput.keyboard import Key, Controller
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = webdriver.EdgeOptions()
+web = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
+web.get('https://slms.sbu.ac.ir/')
+while True:
+    try:
+        web.find_element(by='xpath', value='//*[@id="inputName"]').send_keys('username')
+        web.find_element(by='xpath', value='//*[@id="inputPassword"]').send_keys('password')
+        break
+    except:
+        pass
+WebDriverWait(web, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="submit"]')))
+web.find_element(by='xpath', value='//*[@id="submit"]').click()
+WebDriverWait(web, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="inst43"]/div[2]/ul/li[2]/div/a')))
+web.find_element(by='xpath', value='//*[@id="inst43"]/div[2]/ul/li[2]/div/a').click()
+WebDriverWait(web, 100).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="module-7215"]/div/div/div[2]/div/a/span')))
+web.find_element(by='xpath', value='//*[@id="module-7215"]/div/div/div[2]/div/a/span').click()
+WebDriverWait(web, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="id_submitbutton"]')))
+web.find_element(by='xpath', value='//*[@id="id_submitbutton"]').click()
+WebDriverWait(web, 100).until(EC.presence_of_element_located(
+    (By.XPATH, '//*[@id="launchOptionsDialog"]/div[2]/coral-dialog-content/div[1]/div[2]/div')))
+
+web.find_element(by='xpath',
+                 value='//*[@id="launchOptionsDialog"]/div[2]/coral-dialog-content/div[1]/div[2]/div').click()
+time.sleep(4)
+kb = Controller()
+kb.press(Key.left)
+kb.press(Key.enter)
+time.sleep(10)
