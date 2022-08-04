2c9046a6a2ba191f7ad5b411c6eab628b336f9a4
sadrabera <s.berangi@mail.sbu.ac.ir>
2022-08-04 19:02:07 +0430

    some comments added

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ import time
 
 from pynput.keyboard import Key, Controller
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 options = webdriver.EdgeOptions()
 web = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
 web.get('https://slms.sbu.ac.ir/')
-while True:
+while True:  # this is same thing which WebDriverWait to
     try:
         web.find_element(by='xpath', value='//*[@id="inputName"]').send_keys('username')
         web.find_element(by='xpath', value='//*[@id="inputPassword"]').send_keys('password')
         break
     except:
         pass
-WebDriverWait(web, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="submit"]')))
+WebDriverWait(web, 100).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="submit"]')))  # wait for the page to load
 web.find_element(by='xpath', value='//*[@id="submit"]').click()
 WebDriverWait(web, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="inst43"]/div[2]/ul/li[2]/div/a')))
 web.find_element(by='xpath', value='//*[@id="inst43"]/div[2]/ul/li[2]/div/a').click()
@@ -37,5 +36,5 @@ web.find_element(by='xpath',
 time.sleep(4)
 kb = Controller()
 kb.press(Key.left)
-kb.press(Key.enter)
+kb.press(Key.enter)  # press enter because of alert
 time.sleep(10)
